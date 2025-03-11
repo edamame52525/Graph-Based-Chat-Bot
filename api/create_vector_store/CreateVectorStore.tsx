444128a22e008926feedf576d46913a3156b7e0f
edamame52525 <edamame19820810@gmail.com>
2025-03-11 19:10:47 +0900

    create api (during...)

---
+++ api/create_vector_store/CreateVectorStore.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { NodeData } from '@/types/node_types'
+import { useNode } from '@/components/context/NodeContext'
+
+const CreateVectorStore = (Nodes:NodeData) => {
+    const currentNodes = useNode();
+
+
+
+
+
+  return (
+  )
+}
+
+export default CreateVectorStore
